0fb31048541a470548804e240bc38c88fe88a24b
BoiDontCry <2vain0131@gmail.com>
2023-11-30 13:32:56 +0800

    Site updated: 2023-11-30 13:32:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/11/21/Day1/","2023/11/22/Day2/","2023/11/26/Day4/","2023/11/24/Day3/","2023/11/28/Day5/","2023/11/29/Day6/","2023/11/30/Day7/","2023/11/20/hello-world/","2023/11/28/test-images/","2023/11/28/test-links/","2023/11/30/test-server/"];function toRandomPost(){
+var posts=["2023/11/21/Day1/","2023/11/22/Day2/","2023/11/24/Day3/","2023/11/28/Day5/","2023/11/26/Day4/","2023/11/29/Day6/","2023/11/30/Day7/","2023/11/20/hello-world/","2023/11/28/test-images/","2023/11/28/test-links/","2023/11/30/test-server/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
