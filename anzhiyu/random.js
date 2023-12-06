25de56fba09865f266269502acef47f60aebaa59
zhangwei <2vain0131@gmail.com>
2023-12-06 12:33:34 +0800

    Site updated: 2023-12-06 12:33:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/11/21/Day1/","2023/11/22/Day2/","2023/11/24/Day3/","2023/11/26/Day4/","2023/11/28/Day5/","2023/11/29/Day6/","2023/11/30/Day7-20-18/","2023/11/30/Day7/","2023/12/03/Day8/","2023/12/05/Day9/","2023/11/20/hello-world/","2023/11/28/test-images/","2023/11/28/test-links/","2023/11/30/test-server/","2023/11/30/test-upload/","2023/12/04/testserver/","2023/12/01/毕设Day1/"];function toRandomPost(){
+var posts=["2023/11/21/Day1/","2023/11/22/Day2/","2023/11/24/Day3/","2023/11/26/Day4/","2023/11/28/Day5/","2023/11/29/Day6/","2023/11/30/Day7/","2023/11/30/Day7-20-18/","2023/12/03/Day8/","2023/12/05/Day9/","2023/11/20/hello-world/","2023/11/28/test-images/","2023/11/28/test-links/","2023/11/30/test-server/","2023/11/30/test-upload/","2023/12/04/testserver/","2023/12/01/毕设Day1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
